fc4afbac552878bfa85599552ce22020cb0f0e60
mrjohnbeard <wengerbeard@gmail.com>
2021-11-22 20:41:35 -0800

    added images folder. gameBoard complete

--- verbs.js
+++ verbs.js
@@ -1,100 +1,116 @@
 // This will be a memory game for solo play only. 
 // Basic layout will be 20 cards on a game board, 10 sets of 2 of the same cards; in a 4 column x 5 row style.
 // The cards will have images on both the front (specific Team) and back (generic).
+document.addEventListener('DOMContentLoaded', () => {
 
 const teamNames = [
-    
-    // teamName
 
-    {
-        name: 'Alpha Romeo',
-        img: 'image/.png',
-    },
-    {
-        name: 'Alpha Romeo',
-        img: 'image/.png',
-    },
-    {
-        name: 'Alpha Tauri',
-        img: 'image/.png',
-    },
-    {
-        name: 'Alpha Tauri',
-        img: 'image/.png',
-    },
-    {
-        name: 'Alpine',
-        img: 'image/.png',
-    },
-    {
-        name: 'Alpine',
-        img: 'image/.png',
-    },
-    {
-        name: 'Aston Martin',
-        img: 'image/.png',
-    },
-    {
-        name: 'Aston Martin',
-        img: 'image/.png',
-    },
-    {
-        name: 'Ferrari',
-        img: 'image/.png',
-    },
-    {
-        name: 'Ferrari',
-        img: 'image/.png',
-    },
-    {
-        name: 'Haas',
-        img: 'image/.png',
-    },
-    {
-        name: 'Haas',
-        img: 'image/.png',
-    },
-    {
-        name: 'McLaren',
-        img: 'image/.png',
-    },
-    {
-        name: 'McLaren',
-        img: 'image/.png',
-    },
-    {
-        name: 'Mercedes',
-        img: 'image/.png',
-    },
-    {
-        name: 'Mercedes',
-        img: 'image/.png',
-    },
-    {
-        name: 'Red Bull',
-        img: 'image/.png',
-    },
-    {
-        name: 'Red Bull',
-        img: 'image/.png',
-    },
-    {
-        name: 'Willams',
-        img: 'image/.png',
-    },
-    {
-        name: 'image/.png',
-        img: 'Willams',
-    },   
-]
+        {
+            name: 'Alpha Romeo',
+            image: 'image/AlfaRomeo.png',
+        },
+        {
+            name: 'Alpha Romeo',
+            image: 'image/AlfaRomeo.png',
+        },
+        {
+            name: 'Alpha Tauri',
+            image: 'image/AlphaTauri.png',
+        },
+        {
+            name: 'Alpha Tauri',
+            image: 'image/AlphaTauri.png',
+        },
+        {
+            name: 'Alpine',
+            image: 'image/Alpine.png',
+        },
+        {
+            name: 'Alpine',
+            image: 'image/Alpine.png',
+        },
+        {
+            name: 'Aston Martin',
+            image: 'image/AstonMartin.png',
+        },
+        {
+            name: 'Aston Martin',
+            image: 'image/AstonMartin.png',
+        },
+        {
+            name: 'Ferrari',
+            image: 'image/Ferrari.png',
+        },
+        {
+            name: 'Ferrari',
+            image: 'image/Ferrari.png',
+        },
+        {
+            name: 'Haas',
+            image: 'image/Haas.png',
+        },
+        {
+            name: 'Haas',
+            image: 'image/Haas.png',
+        },
+        {
+            name: 'McLaren',
+            image: 'image/McLaren.png',
+        },
+        {
+            name: 'McLaren',
+            image: 'image/McLaren.png',
+        },
+        {
+            name: 'Mercedes',
+            image: 'image/Mercedes.png',
+        },
+        {
+            name: 'Mercedes',
+            image: 'image/Mercedes.png',
+        },
+        {
+            name: 'Red Bull',
+            image: 'image/Red_Bull.png',
+        },
+        {
+            name: 'Red Bull',
+            image: 'image/Red_Bull.png',
+        },
+        {
+            name: 'Willams',
+            image: 'image/Willams.png',
+        },
+        {
+            name: 'Willams',
+            image: 'image/Willams.png',
+        }
+    ];
+
+
+
 
 // Create Game Board
-function createBoard() 
-    for (let i = 0; i < teamNames.length; i++);
+
+const board = document.querySelector('.gameBoard');
+
+function createBoard() { 
+    for (let i = 0; i < teamNames.length; i++) {
+        var card = document.createElement('img');
+        card.setAttribute('src', 'images/realLogo.png');
+        card.setAttribute('date-id', i);
+        board.appendChild(card);
+        }
+    }
+
+    createBoard();
+
+})    
+
 
 
 // Shuffle
-teamNames.sort(() => 0.25 - Math.random())
+// teamNames.sort(() => 0.5 - Math.random())
 
 // Cards Face Down
 
